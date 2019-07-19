ef64b05baf8159d0248bd0726402fc6e031d10cd
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-07-19 15:00:16 -0700

    Implement message service and update message component HTML

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -3,14 +3,17 @@ import { Observable, of } from 'rxjs';
 
 import { ToDo } from './to-do';
 import { TODOS } from './mock-to-dos';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
   getTodos(): Observable<ToDo[]> {
+    // TODO: send the message _after_ fetching the todos
+    this.messageService.add('TodoService: fetched todos');
     return of(TODOS);
   }
 
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 }
