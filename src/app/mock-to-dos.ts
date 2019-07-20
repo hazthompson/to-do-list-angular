a7be93b13629299225f414b7bc2f13b2312edd58
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-07-20 14:06:21 -0700

    Add to mock-todo list

--- src/app/mock-to-dos.ts
+++ src/app/mock-to-dos.ts
@@ -2,10 +2,11 @@ import { ToDo } from './to-do';
 
 export const TODOS: ToDo[] = [
   { id: 11, name: 'TypeScript' },
-  { id: 12, name: 'React Native' },
+  { id: 12, name: 'TensorFlow' },
   { id: 13, name: 'Flutter' },
-  { id: 14, name: 'PHP' },
-  { id: 15, name: 'Vive' },
-  { id: 16, name: 'PWAs' },
-  { id: 17, name: 'Ant Design/Material-UI' }
+  { id: 14, name: 'React Native' },
+  { id: 15, name: 'PHP' },
+  { id: 16, name: 'Vive' },
+  { id: 17, name: 'PWAs' },
+  { id: 18, name: 'Ant Design/Material-UI' }
 ];
