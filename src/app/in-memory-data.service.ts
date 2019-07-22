6210788b5e367250ddfb16088e5c33041687f891
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-07-22 15:32:25 -0700

    add resource and project links to details page

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -15,7 +15,14 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 15, name: 'PHP' },
       { id: 16, name: 'Vive' },
       { id: 17, name: 'PWAs' },
-      { id: 18, name: 'Ant Design/Material-UI' }
+      { id: 18, name: 'Ant Design/Material-UI' },
+      {
+        id: 19,
+        name: 'ReactJS',
+        link: 'https://reactjs.org/',
+        project: 'Jetify',
+        projectLink: 'https://github.com/hazthompson/Jetify'
+      }
     ];
     return { todos };
   }
