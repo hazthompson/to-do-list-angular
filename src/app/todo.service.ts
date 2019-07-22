2b56871aa0f4911ac4f9ad95baf603b36ebec94d
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-07-21 22:34:19 -0700

    add error handling

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 import { ToDo } from './to-do';
-import { TODOS } from './mock-to-dos';
+
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,13 +14,18 @@ export class TodoService {
   getTodos(): Observable<ToDo[]> {
     // TODO: send the message _after_ fetching the todos
     this.messageService.add('TodoService: fetched todos');
-    return this.http.get<ToDo[]>(this.todosUrl);
+    return this.http
+      .get<ToDo[]>(this.todosUrl)
+      .pipe(catchError(this.handleError<ToDo[]>('getTodos', [])));
   }
 
   getTodo(id: number): Observable<ToDo> {
     // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`TodoService: fetched todo id=${id}`);
-    return of(TODOS.find(todo => todo.id === id));
+    const url = `${this.todosUrl}/${id}`;
+    return this.http.get<ToDo>(url).pipe(
+      tap(_ => this.log(`fetched todo id=${id}`)),
+      catchError(this.handleError<ToDo>(`getTodo id=${id}`))
+    );
   }
 
   private log(message: string) {
@@ -27,6 +33,25 @@ export class TodoService {
   }
 
   private todosUrl = 'api/todos'; // URL to web api
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 
   constructor(
     private http: HttpClient,
